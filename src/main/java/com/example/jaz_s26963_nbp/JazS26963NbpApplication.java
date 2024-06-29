3737c7aa9ab3c01e6df0c590f2658826953b4994
s26963 <s26963@pjwstk.edu.pl>
2024-06-29 17:40:06 +0200

    first commit

---
+++ src/main/java/com/example/jaz_s26963_nbp/JazS26963NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s26963_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS26963NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS26963NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jaz_s26963_nbp/JazS26963NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s26963_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS26963NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
